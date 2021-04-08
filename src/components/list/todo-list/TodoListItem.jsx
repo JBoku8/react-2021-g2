6575fd2aab43893b387bbcb793c9e35871ef50f4
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-04-08 20:39:10 +0400

    todo provider, api service

--- src/components/list/todo-list/TodoListItem.jsx
+++ src/components/list/todo-list/TodoListItem.jsx
@@ -1,11 +1,24 @@
-function TodoListItem({ todo, onClick }) {
+import { useContext, useEffect } from 'react';
+
+import { TodoProvider } from '../../../providers/TodoProvider';
+import API_SERVICE from '../../../services/api';
+
+function TodoListItem({ todo }) {
+  const { onTodoUpdate } = useContext(TodoProvider);
+
+  useEffect(() => {
+    (async () => {
+      const item = await API_SERVICE.getTodoListItemAsync(todo.id);
+      console.log('[TodoListItem]', item);
+    })();
+  }, [todo]);
+
   return (
     <div
       className={`list-group-item list-group-item-action ${
-        todo.completed ? "list-group-item-primary" : ""
+        todo.completed ? 'list-group-item-primary' : ''
       }`}
-      onClick={() => onClick(todo)}
-    >
+      onClick={() => onTodoUpdate(todo)}>
       {todo.title}
     </div>
   );
